c21425ae6fca7027697df49682e0cab3e298a868
Mallory PEULET <mpeulet@made-f0Ar9s2.clusters.42paris.fr>
2023-11-13 18:16:47 +0100

    cpp00 ex00

---
+++ module00/ex00/src/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 15:19:17 by mpeulet           #+#    #+#             */
+/*   Updated: 2023/11/13 17:08:25 by mpeulet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+std::string to_uppercase(std::string s)
+{
+    for (std::size_t i = 0; i < s.length(); ++i)
+        s[i] = std::toupper(s[i]);
+    return (s);
+}
+
+int	main(int ac, char **av)
+{
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        for (int i = 1; i < ac; i++)
+            std::cout << to_uppercase(av[i]);
+    }
+    std::cout << std::endl;
+    return (0);
+}

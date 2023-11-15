f0804e5d88266bd2c37516dea1a59eb4979b42cd
root <root@Asus-MaloP47>
2023-11-15 14:02:21 +0100

    Phone book draft

--- module00/ex01/src/main.cpp
+++ module00/ex01/src/main.cpp
@@ -25,19 +25,3 @@ int	main(void)
 		program_runs = phone_book.functions( user_input );
 	return (0);
 }
-
-/*int	main(void)
-{
-	std::string user_input;
-
-	for (; std::getline(std::cin, user_input) ;)
-		if (!user_input.compare("EXIT") || std::cin.eof())
-			break ;
-		else if (!user_input.compare("ADD"))
-			std::cout << "ADD fn" << std::endl;
-		else if (!user_input.compare("SEARCH"))
-			std::cout << "SEARCH fn" << std::endl;
-		else
-			std::cout << "Valid commands : ADD SEARCH EXIT" << std::endl;
-	return (0);
-}*/

b8c84139059bfe7ed2f5b5135516721f8108eb24
Mallory PEULET <mpeulet@made-f0Br2s5.clusters.42paris.fr>
2023-11-27 14:34:00 +0100

    opening files tests

--- module01/ex04/src/main.cpp
+++ module01/ex04/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 16:10:18 by mpeulet           #+#    #+#             */
-/*   Updated: 2023/11/27 11:14:35 by mpeulet          ###   ########.fr       */
+/*   Updated: 2023/11/27 14:32:47 by mpeulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 #include <iostream>
 
 # define EXAMPLE "\033[0;91mParameters must be 3 : <filename> <string to be replaced> <string to replace by>\033[0;39m"
+# define EMPTY "Arguments strings must not be empty"
+
+# define INVALID_EMPTY std::cout << EMPTY << std::endl
 # define INVALID_ARG std::cout << EXAMPLE << std::endl
 # define RUNTIME_ERR std::cerr << "Error: " << err.what() << std::endl
 
 int	main(int ac, char **av) {
 
-	if (ac != 4) return INVALID_ARG, 1;
+	if (ac != 4) return INVALID_ARG, 1 ;
+	if (std::string(av[2]).empty() || std::string(av[3]).empty()) return INVALID_EMPTY, 1 ;
 	
 	std::string	infile = av[1];
 	std::string outfile = infile + ".replace";

33db9635dab39ba4b0c0bbf28c8d980b12219ae8
Mallory PEULET <mpeulet@gmail.com>
2024-02-21 16:31:24 +0100

    quick save, need to fix char with zero value or else, probably with std::atoi

--- module06/ex00/src/ScalarConverter.cpp
+++ module06/ex00/src/ScalarConverter.cpp
@@ -6,19 +6,11 @@
 /*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 12:40:46 by mpeulet           #+#    #+#             */
-/*   Updated: 2024/02/21 13:38:13 by mpeulet          ###   ########.fr       */
+/*   Updated: 2024/02/21 16:29:24 by mpeulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
-
-/* *** public functions *** */
-
-ScalarConverter::~ScalarConverter( void ) {}
-
-void	ScalarConverter::convert( const std::string & literal ) {
-	std::cout << literal << std::endl ;
-}
 
 /* *** private functions *** */
 
@@ -33,43 +25,51 @@ ScalarConverter & ScalarConverter::operator=( ScalarConverter const & rhs ) {
 	return *this ;
 }
 
-bool	ScalarConverter::isChar( const std::string & literal ) {
-	return literal.size() == 1 && isprint( literal ) ;
+bool	ScalarConverter::isChar( const std::string & literal, char *c) {
+	if ( literal.size() ==  3 && literal[0] == 39 && literal[2] == 39 ) {
+		*c = static_cast<char>( literal[1] );
+		return true ;
+	}
+	else if ( literal.size() == 1 ) {
+		*c = static_cast<char>( literal[0] );
+		return true ;
+	}
+	return false ;
 }
 
-bool	ScalarConverter::isInt( const std::string & literal ) {
+void	ScalarConverter::fromChar( char c ) {
+	if ( !isprint( c ) ) {
+		std::cout << "char c: Non displayable\n"
+					<< "int: " << static_cast<int>( c ) << "\n"
+					<< "float: " << static_cast<float>( c ) << "\n"
+					<< "double: " << static_cast<double>( c ) << "\n" ;
+	}
+	else
+		std::cout << "char c: " << c << "\n"
+					<< "int: " << static_cast<int>( c ) << "\n"
+					<< "float: " << static_cast<float>( c ) << "\n"
+					<< "double: " << static_cast<double>( c ) << "\n" ;
+}
+
+// bool	ScalarConverter::isInt( const std::string & literal, int *n ) {
 	
-}
+// }
 
-bool	ScalarConverter::isFloat( const std::string & literal ) {
+// bool	ScalarConverter::isFloat( const std::string & literal, float *n ) {
 	
-}
+// }
 
-bool	ScalarConverter::isDouble( const std::string & literal ) {
-	
-}
+/* *** public functions *** */
 
-bool	ScalarConverter::isPseudoLiteral( const std::string & literal ) {
-	return ( literal == "-inff" || literal == "+inff" || literal == "nanf"
-		|| literal == "-inf" || literal == "+inf" || literal == "nan") ;
-}
+ScalarConverter::~ScalarConverter( void ) {}
 
-void	ScalarConverter::charToElse( char c ) const {
-	
-}
-
-void	ScalarConverter::intToElse( int n ) const {
-	
-}
-
-void	ScalarConverter::floatToElse( float f ) const {
-	
-}
-
-void	ScalarConverter::doubleToElse( double d ) const {
-	
-}
-
-void *	ScalarConverter::stringToCast( const std::string & literal ) {
-	
+void	ScalarConverter::convert( const std::string & literal ) {
+	char	c ;
+	// int		n ;
+	// float	f ;
+	// double	d ;
+	if ( isChar( literal, &c ) ) {
+		fromChar( c ) ;
+		return ;
+	}
 }
--- module06/ex00/src/main.cpp
+++ module06/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 13:36:51 by mpeulet           #+#    #+#             */
-/*   Updated: 2024/02/20 15:11:40 by mpeulet          ###   ########.fr       */
+/*   Updated: 2024/02/21 16:10:04 by mpeulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 int	main( int ac, char** av ) {
 
 	// ScalarConverter	Test;
-	if ( ac ==  2 )
+	if ( ac == 2 && static_cast<std::string>( av[1] ).size() )
 		ScalarConverter::convert( av[1] ) ;
 	else
 		std::cout << EXAMPLE ;
-	
+
 	return 0 ;
 }

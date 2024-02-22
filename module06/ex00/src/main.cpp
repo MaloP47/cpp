798e566cba1b1fa4f63199b1f4173b50c8a7af5f
Mallory PEULET <mpeulet@gmail.com>
2024-02-22 17:09:18 +0100

    cpp6ex00 complete with tests, the pain was reel

--- module06/ex00/src/main.cpp
+++ module06/ex00/src/main.cpp
@@ -6,19 +6,54 @@
 /*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 13:36:51 by mpeulet           #+#    #+#             */
-/*   Updated: 2024/02/21 16:10:04 by mpeulet          ###   ########.fr       */
+/*   Updated: 2024/02/22 17:07:28 by mpeulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-int	main( int ac, char** av ) {
+int	main( void ) {
 
-	// ScalarConverter	Test;
-	if ( ac == 2 && static_cast<std::string>( av[1] ).size() )
-		ScalarConverter::convert( av[1] ) ;
-	else
-		std::cout << EXAMPLE ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "0" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "nan" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "42.0f" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "'0'" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "97f" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "424242.0f" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "-42424.0f" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "42424" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "2147483647" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "-2147483648" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "2147483648" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "122" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "90" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "170" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "2147483648.0" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "122.0" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "90.0" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "170.0" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "Hello" ) ;
+	std::cout << "-------------------\n" ;
+	ScalarConverter::convert( "" ) ;
 
 	return 0 ;
 }

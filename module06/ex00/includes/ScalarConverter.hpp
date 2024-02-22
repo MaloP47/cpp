4820c96a1fd7df594750e2816be496f93430c150
Mallory PEULET <mpeulet@gmail.com>
2024-02-22 17:09:18 +0100

    cpp6ex00 complete with tests, the pain was reel

--- module06/ex00/includes/ScalarConverter.hpp
+++ module06/ex00/includes/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 12:41:28 by mpeulet           #+#    #+#             */
-/*   Updated: 2024/02/21 15:55:16 by mpeulet          ###   ########.fr       */
+/*   Updated: 2024/02/22 16:59:59 by mpeulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,23 @@
 
 # include <iostream>
 # include <cctype>
+# include <cstdlib>
+# include <iomanip>
+# include <limits>
+# include <climits>
 
 # define EXAMPLE "Program needs 1 argument only, example :\n\
 ./Scalar_types 42.42\n"
+
+enum literalType {
+	NONE,
+    PSEUDO,
+    CHAR,
+    INT,
+    FLOAT,
+    DOUBLE,
+    INVALID 
+} ;
 
 class	ScalarConverter {
 
@@ -33,12 +47,17 @@ class	ScalarConverter {
 
 		static bool	isChar( const std::string & literal, char *c) ;
 		static bool	isInt( const std::string & literal, int *n ) ;
-		static bool	isFloat( const std::string & literal, float *n ) ;
+		static bool	isFloat( const std::string & literal, float *f ) ;
+		static bool	isDouble( const std::string & literal, double *d ) ;
+		static bool	isPseudo( const std::string & literal, float *f, double * d ) ;
 
 		static void	fromChar( char c ) ;
 		static void	fromInt( int n ) ;
 		static void	fromFloat( float f ) ;
 		static void	fromDouble( double d ) ;
+		static void fromPseudo( float * f, double * d ) ;
+
+		static literalType	detemineType( const std::string & literal, char *c, int *n, float *f, double *d ) ;
 
 } ;
 
--- module06/ex00/src/ScalarConverter.cpp
+++ module06/ex00/src/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 12:40:46 by mpeulet           #+#    #+#             */
-/*   Updated: 2024/02/21 16:29:24 by mpeulet          ###   ########.fr       */
+/*   Updated: 2024/02/22 17:06:59 by mpeulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,49 +27,183 @@ ScalarConverter & ScalarConverter::operator=( ScalarConverter const & rhs ) {
 
 bool	ScalarConverter::isChar( const std::string & literal, char *c) {
 	if ( literal.size() ==  3 && literal[0] == 39 && literal[2] == 39 ) {
-		*c = static_cast<char>( literal[1] );
+		*c = static_cast<char>( literal[1] ) ;
 		return true ;
 	}
 	else if ( literal.size() == 1 ) {
-		*c = static_cast<char>( literal[0] );
+		*c = static_cast<char>( literal[0] ) - 48 ;
 		return true ;
 	}
 	return false ;
 }
 
 void	ScalarConverter::fromChar( char c ) {
-	if ( !isprint( c ) ) {
-		std::cout << "char c: Non displayable\n"
+	if ( !std::isprint( c ) ) {
+		std::cout << "char : Non displayable\n"
 					<< "int: " << static_cast<int>( c ) << "\n"
-					<< "float: " << static_cast<float>( c ) << "\n"
-					<< "double: " << static_cast<double>( c ) << "\n" ;
+					<< "float: " << std::fixed << std::setprecision( 1 ) << static_cast<float>( c ) << "f\n"
+					<< "double: " << std::fixed << std::setprecision( 1 ) << static_cast<double>( c ) << "\n" ;
 	}
 	else
-		std::cout << "char c: " << c << "\n"
+		std::cout << "char : " << "\'" << c << "\'\n"
 					<< "int: " << static_cast<int>( c ) << "\n"
-					<< "float: " << static_cast<float>( c ) << "\n"
-					<< "double: " << static_cast<double>( c ) << "\n" ;
+					<< "float: " << std::fixed << std::setprecision( 1 ) << static_cast<float>( c ) << "f\n"
+					<< "double: " << std::fixed << std::setprecision( 1 ) << static_cast<double>( c ) << "\n" ;
 }
 
-// bool	ScalarConverter::isInt( const std::string & literal, int *n ) {
-	
-// }
+bool	ScalarConverter::isInt( const std::string & literal, int *n ) {
+	char	*endptr ;
+	long	nb = std::strtol( literal.c_str(), &endptr, 10 ) ;
+	if ( endptr == literal.c_str() || *endptr != 0 )
+		return false ;
+	if ( nb > INT_MAX || nb < INT_MIN )
+		return false ;
+	*n = nb ;
+	return true ;
+}
 
-// bool	ScalarConverter::isFloat( const std::string & literal, float *n ) {
-	
-// }
+void	ScalarConverter::fromInt( int n ) {
+	if ( n >= std::numeric_limits<char>::min() && n <= std::numeric_limits<char>::max() ) {
+		if ( std::isprint( static_cast<int>( n ) ) )
+			std::cout << "char : " << "\'" << static_cast<char>( n ) << "\'\n" ;
+		else
+			std::cout << "char : Non displayable\n" ;
+	}
+	else
+		std::cout << "char: impossible\n" ;
+	std::cout << "int: " << n << "\n" ;
+	std::cout << "float: " << std::fixed << std::setprecision( 1 ) << static_cast<float>( n ) << "f\n" ;
+	std::cout << "double: " << std::fixed << std::setprecision( 1 ) << static_cast<double>( n ) << "\n" ;
+}
+
+bool	ScalarConverter::isFloat( const std::string & literal, float *f ) {
+	char		*endptr ;
+	*f = std::strtod( literal.c_str(), &endptr ) ;
+	if ( endptr == literal.c_str() || *endptr != 'f' )
+		return false ;
+	if ( *endptr == 'f' && ( endptr + 1 && *( endptr + 1 ) != 0 ) )
+		return false ;
+	*f = std::atof( literal.c_str() ) ;
+	return true ;
+}
+
+void	ScalarConverter::fromFloat( float f ) {
+	if ( f >= std::numeric_limits<char>::min() && f <= std::numeric_limits<char>::max() ) {
+		if ( std::isprint( static_cast<int>( f) ) )
+			std::cout << "char : " << "\'" << static_cast<char>( f ) << "\'\n" ;
+		else
+			std::cout << "char : Non displayable\n" ;
+	}
+	else
+		std::cout << "char: impossible\n" ;
+	if ( f > std::numeric_limits<int>::min() -1.0f && f < std::numeric_limits<int>::max() + 1.0f ) 
+		std::cout << "int: " << static_cast<int>( f ) << "\n" ;
+	else 
+		std::cout << "int: impossible\n" ;
+	std::cout << "float: " << std::fixed << std::setprecision( 1 ) << f << "f\n" ;
+	std::cout << "double: " << std::fixed << std::setprecision( 1 ) << static_cast<double>( f ) << "\n" ;
+}
+
+bool	ScalarConverter::isDouble( const std::string & literal, double * d ) {
+	char	*endptr ;
+	size_t	pos = literal.find( '.' ) ;
+	if ( pos == std::string::npos )
+		return false ;
+	*d = std::strtold( literal.c_str(), &endptr ) ;
+	if ( endptr == literal.c_str() || *endptr != 0 )
+		return false ;
+	return true ;
+}
+
+void	ScalarConverter::fromDouble( double d ) {
+	if ( d >= std::numeric_limits<char>::min() && d <= std::numeric_limits<char>::max() ) {
+		if ( std::isprint( static_cast<int>( d ) ) )
+			std::cout << "char : " << "\'" << static_cast<char>( d ) << "\'\n" ;
+		else
+			std::cout << "char : Non displayable\n" ;
+	}
+	else
+		std::cout << "char: impossible\n" ;
+	if ( d > std::numeric_limits<int>::min() -1.0 && d < std::numeric_limits<int>::max() + 1.0 ) 
+		std::cout << "int: " << static_cast<int>( d ) << "\n" ;
+	else 
+		std::cout << "int: impossible\n" ;
+	if ( d >= std::numeric_limits<float>::min() && d < std::numeric_limits<float>::max() ) 
+		std::cout << "float: " << std::fixed << std::setprecision( 1 ) << static_cast<float>( d ) << "f\n" ;
+	else 
+		std::cout << "float: impossible\n" ;
+	std::cout << "double: " << std::fixed << std::setprecision( 1 ) << d << "\n" ;
+}
+
+bool	ScalarConverter::isPseudo( const std::string & literal, float *f, double * d ) {
+	if ( literal == "inf" || literal == "+inf" || literal == "inff" || literal == "+inff" ) {
+		*f = std::numeric_limits<float>::infinity() ;
+		*d = std::numeric_limits<double>::infinity() ;
+		return true ;
+	}
+	if ( literal == "-inf" || literal == "-inff" ) {
+		*f = -std::numeric_limits<float>::infinity() ;
+		*d = -std::numeric_limits<double>::infinity() ;
+		return true ;
+	}
+	if ( literal == "nan" || literal == "nanf" ) {
+		*f = std::numeric_limits<float>::quiet_NaN() ;
+		*d = std::numeric_limits<double>::quiet_NaN() ;
+		return true ;
+	}
+	return false ;
+}
+
+void	ScalarConverter::fromPseudo( float * f, double * d ) {
+	std::cout << "char: impossible\n"
+				<< "int: impossible\n"
+				<< "float: " <<  *f << "f\n"
+				<< "double: " <<  *d << "\n" ;
+}
+
+literalType	ScalarConverter::detemineType( const std::string & literal , char *c, int *n, float *f, double *d) {
+	 if (literal.empty()) return NONE;
+    else if (isPseudo(literal, f, d)) return PSEUDO;
+    else if (isChar(literal, c)) return CHAR;
+    else if (isInt(literal, n)) return INT;
+    else if (isFloat(literal, f)) return FLOAT;
+    else if (isDouble(literal, d)) return DOUBLE;
+    else return INVALID;
+} 
 
 /* *** public functions *** */
 
 ScalarConverter::~ScalarConverter( void ) {}
 
 void	ScalarConverter::convert( const std::string & literal ) {
-	char	c ;
-	// int		n ;
-	// float	f ;
-	// double	d ;
-	if ( isChar( literal, &c ) ) {
-		fromChar( c ) ;
-		return ;
-	}
+	
+	char	c = 0 ;
+	int		n = 0;
+	float	f = 0;
+	double	d = 0;
+
+	literalType type = detemineType( literal, &c, &n, &f, &d ) ;
+
+	 switch (type) {
+        case NONE:
+            std::cout << "Needs at least one character to use properly.\n";
+            break;
+        case PSEUDO:
+            fromPseudo( &f, &d ) ;
+            break;
+        case CHAR:
+			fromChar( c ) ;
+            break;
+        case INT:
+			fromInt( n ) ;
+            break;
+        case FLOAT:
+            fromFloat( f ) ;
+            break;
+        case DOUBLE:
+            fromDouble( d ) ;
+            break;
+        default:
+            std::cout << "Wrong format or possible overflow.\n" ;
+    }
 }

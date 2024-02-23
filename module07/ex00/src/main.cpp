e13a7c5b7a89d24d0fab3017f8537d596580494b
Mallory PEULET <mpeulet@gmail.com>
2024-02-23 12:23:09 +0100

    cpp7ex00 complete

---
+++ module07/ex00/src/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 17:39:43 by mpeulet           #+#    #+#             */
+/*   Updated: 2024/02/22 17:41:22 by mpeulet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+#include <iostream>
+
+int main( void ) {
+
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return 0;
+
+}

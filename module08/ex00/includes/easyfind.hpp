71d254fd55cf93fe5efb223984c2c8b06f2db84d
Mallory PEULET <mpeulet@gmail.com>
2024-02-29 15:31:06 +0100

    cpp08ex00 finished with tests

--- module08/ex00/includes/easyfind.hpp
+++ module08/ex00/includes/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 13:51:51 by mpeulet           #+#    #+#             */
-/*   Updated: 2024/02/26 15:06:15 by mpeulet          ###   ########.fr       */
+/*   Updated: 2024/02/29 15:09:40 by mpeulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,19 @@
 # include <algorithm>
 # include <exception>
 
+class	NotFound : public std::exception {
+	public:
+		virtual const char * what( void ) const throw() {
+			return "Nb not found." ;
+		}
+} ;
+
 template< typename T >
 typename T::const_iterator	easyfind( T const & type, int const nb ) {
 		typename T::const_iterator	it = std::find( type.begin(), type.end(), nb ) ;
-		return it != type.end() ? it : throw std::exception ;
+		if ( it != type.end() )
+			return it ;
+		else {
+			throw ::NotFound() ;
+		}
 }
--- module08/ex00/src/main.cpp
+++ module08/ex00/src/main.cpp
@@ -6,25 +6,106 @@
 /*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 14:56:36 by mpeulet           #+#    #+#             */
-/*   Updated: 2024/02/26 15:06:21 by mpeulet          ###   ########.fr       */
+/*   Updated: 2024/02/29 15:29:24 by mpeulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
-#include <stack>
 #include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
 
 int	main( void ) {
 
-	std::stack<int>	st;
+	{
+		std::vector<int>	vec;
 
-	st.push(0);
-    st.push(1);
-    st.push(2);
-    st.push(3);
-    st.push(4);
+		vec.push_back(0);
+    	vec.push_back(1);
+    	vec.push_back(2);
+    	vec.push_back(2);
+    	vec.push_back(3);
 
-	std::cout << *::easyfind( st, 3 ) << std::endl ;
+		try {
+			std::vector<int>::const_iterator it_begin = vec.begin();
+			std::vector<int>::const_iterator it = ::easyfind(vec, 3);
+			if (it != vec.end()) {
+    			std::cout << "Nb found at index: " << std::distance(it_begin, it) << std::endl;
+    			std::cout << "At index value is: " << * ::easyfind(vec, 3) << std::endl;
+			}
+		}
+		catch( std::exception & e ) {
+			std::cout << e.what() << std::endl ;		
+		}
+		try {
+			std::vector<int>::const_iterator it_begin = vec.begin();
+			std::vector<int>::const_iterator it = ::easyfind(vec, 8);
+			if (it != vec.end()) {
+    			std::cout << "Nb found at index: " << std::distance(it_begin, it) << std::endl;
+    			std::cout << "At index value is: " << * ::easyfind(vec, 8) << std::endl;
+			}
+		}
+		catch( std::exception & e ) {
+			std::cout << e.what() << std::endl ;		
+		}
+
+	}
+	std::cout << "----------------------------------\n" ;
+	{
+		std::list<int>	lst;
+
+		lst.push_back(0);
+    	lst.push_back(1);
+    	lst.push_back(2);
+    	lst.push_back(3);
+    	lst.push_back(4);
+		lst.push_back(0);
+    	lst.push_back(1);
+    	lst.push_back(2);
+    	lst.push_back(3);
+    	lst.push_back(4);
+		lst.push_back(0);
+    	lst.push_back(1);
+    	lst.push_back(2);
+    	lst.push_back(8);
+    	lst.push_back(4);
+
+		try {
+			std::list<int>::const_iterator it_begin = lst.begin();
+			std::list<int>::const_iterator it = ::easyfind(lst, 8);
+			if (it != lst.end()) {
+    			std::cout << "Nb found at index: " << std::distance(it_begin, it) << std::endl;
+    			std::cout << "At index value is: " << * ::easyfind(lst, 8) << std::endl;
+			}
+		}
+		catch( std::exception & e ) {
+			std::cout << e.what() << std::endl ;		
+		}
+	}
+	std::cout << "----------------------------------\n" ;
+	{
+		std::deque<int>	dq;
+
+		dq.push_back(8);
+		dq.push_back(8);
+		dq.push_back(8);
+		dq.push_back(8);
+		dq.push_back(8);
+
+
+		try {
+			std::deque<int>::const_iterator it_begin = dq.begin();
+			std::deque<int>::const_iterator it = ::easyfind(dq, 8);
+			if (it != dq.end()) {
+    			std::cout << "Nb found at index: " << std::distance(it_begin, it) << std::endl;
+    			std::cout << "At index value is: " << * ::easyfind( dq, 8) << std::endl;
+			}
+		}
+		catch( std::exception & e ) {
+			std::cout << e.what() << std::endl ;		
+		}
+	}
 
 	return 0 ;
 }
